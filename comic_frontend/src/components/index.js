66cd586ad72be364574c39dede316c39e4160688
Mmmtttt <mt1511318385@gmail.com>
2026-03-01 01:19:06 +0800

    前端重构总结

--- comic_frontend/src/components/index.js
+++ comic_frontend/src/components/index.js
@@ -3,11 +3,6 @@
  * 集中管理所有组件
  */
 
-// 阅读器组件
-export { default as ReaderToolbar } from './reader/ReaderToolbar.vue'
-export { default as ReaderProgress } from './reader/ReaderProgress.vue'
-export { default as ReaderSettings } from './reader/ReaderSettings.vue'
-
 // 漫画组件
 export { default as ComicCard } from './comic/ComicCard.vue'
 export { default as ComicGrid } from './comic/ComicGrid.vue'
@@ -19,5 +14,3 @@ export { default as TagFilter } from './tag/TagFilter.vue'
 // 通用组件
 export { default as LoadingSpinner } from './common/LoadingSpinner.vue'
 export { default as EmptyState } from './common/EmptyState.vue'
-export { default as ImageLoader } from './common/ImageLoader.vue'
-export { default as CacheManager } from './common/CacheManager.vue'
--- comic_frontend/src/main.js
+++ comic_frontend/src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import pinia from './store'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import Vant, { Lazyload } from 'vant'
 import 'vant/lib/index.css'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
 app.use(router)
